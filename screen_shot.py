5cc9735bd1b4361862b1fb664e113f3776916f2a
Abhishek Dubey <abhishek.ash046@gmail.com>
2020-07-12 08:16:19 +0530

    Add to generate random number

--- screen_shot.py
+++ screen_shot.py
@@ -3,8 +3,10 @@ import pyautogui
 
 
 def screens_shot():
+    name = int(round(time.time()*1000))
+    name = '{}.png'.format(name)
     time.sleep(5)
-    img = pyautogui.screenshot('test.png')
+    img = pyautogui.screenshot(name)
     img.show()
 
 
